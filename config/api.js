38946f466be8049fe284fa09908693ed0630bda2
Carine <carinerandrianarijaona@gmail.com>
2025-06-04 15:02:29 +0200

    add: starting connect front and back

--- config/api.js
+++ config/api.js
@@ -1,7 +1,1 @@
-const API_CONFIG = {
-    baseURL: 'http://127.0.0.1:8000', // symfony web server URL
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-};
+
