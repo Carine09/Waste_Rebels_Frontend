84a07cf5eab6d48c6c3857a4e54b5ff00cbba3b2
Leonie Miege <leonie.miege@gmail.com>
2025-06-04 14:04:35 +0200

    add : setting up html link to stylesheets and script

---
+++ config/api.js
@@ -0,0 +1,7 @@
+const API_CONFIG = {
+    baseURL: 'http://127.0.0.1:8000', // symfony web server URL
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+};
